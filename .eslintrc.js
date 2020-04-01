f493112b1f111cdc30d0801ac7c49b92b834098c
jeffersonshibuya@yahoo.com.br <jeffersonshibuya@yahoo.com.br>
2020-04-01 11:37:06 -0300

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extension: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reasign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+  },
+};
